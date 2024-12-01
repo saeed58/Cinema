bde2dc4a08f7d982479468cfaec12a349d8fb242
Saeed Shokrzadeh -Home <s.shokrzadeh@gmail.com>
2024-12-01 09:07:11 +0330

    Create Responsive area

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import { Button } from "@/components/ui/button"
-import { HStack } from "@chakra-ui/react"
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <HStack>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
-    </HStack>
-  )
-}
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        md: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{ 
+        base : "1fr",
+        md : "250px 1fr"
+       }}
+      gap="0"
+    >
+      <GridItem area='nav' bg="red">Nav</GridItem>
+
+      <GridItem area='aside' bg="orange" hideBelow="md">
+        aside
+      </GridItem>
+
+      <GridItem area='main' bg="blue">main</GridItem>
+    </Grid>
+  );
+};
 
 export default App;
