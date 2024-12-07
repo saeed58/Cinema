ab51fa9e3640eda7eb4b84cc752f3fb2eb5a21b5
Saeed Shokrzadeh -Home <s.shokrzadeh@gmail.com>
2024-12-07 12:25:08 +0330

    impelemnt ininite query

--- src/components/MovieAllList.tsx
+++ src/components/MovieAllList.tsx
@@ -1,29 +1,33 @@
-import { SimpleGrid ,Text} from "@chakra-ui/react";
-import { useQuery } from "@tanstack/react-query";
+import { Button, SimpleGrid, Text, useStatusStyles } from "@chakra-ui/react";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import MovieCard from "@/components/MovieCard";
 import MovieCardContainer from "@/components/MovieCardContainer";
 import MovieSkeleon from "@/components/MovieSkeleon";
 import { apiQuery } from "../entities/apiQuery";
 import { Results } from "@/entities/Results";
+import React, { useState } from "react";
 
 interface Props {
   selectedGenre: Results | null;
-  searchText : String | null;
+  searchText: String | null;
 }
-const Skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-const MovieAllList = ( {selectedGenre , searchText} : Props) => {
-  const fetchMovies = () =>
+const Skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const pageSize = 10;
+const MovieAllList = ({ selectedGenre, searchText }: Props) => {
+  const fetchMovies = ({ pageParam = 0 }) =>
     axios
       .get<apiQuery>("https://searchia.ir/api/index/movie", {
         params: {
-          query: searchText? searchText : '',
-          facets:'country,genre',
-          filters:'is_active:1',
-          facetFilters: selectedGenre ? 'genre:'+selectedGenre?.source.name_fa: '',
-
-          size: 20,
+          query: searchText ? searchText : "",
+          facets: "country,genre",
+          filters: "is_active:1",
+          facetFilters: selectedGenre
+            ? "genre:" + selectedGenre?.source.name_fa
+            : "",
+          from: pageParam,
+          size: pageSize,
           sorts: "publish_date<DESC>",
         },
         headers: {
@@ -32,35 +36,59 @@ const MovieAllList = ( {selectedGenre , searchText} : Props) => {
       })
       .then((res) => res.data.entity);
 
-  const { data, isPending } = useQuery({
-    queryKey: ["movie",selectedGenre,searchText],
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+    isPending,
+  } = useInfiniteQuery({
+    queryKey: ["movie", selectedGenre, searchText],
     queryFn: fetchMovies,
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => {
+      return (lastPage.totalHits / pageSize) > allPages.length
+        ? allPages.length +pageSize-1
+        : undefined;
+    },
     staleTime: 24 * 60 * 60 * 1000, //24h
   });
-  const totalHint = data?.totalHits;
-
-  const MovieResult=data?.results;
-
+  
   return (
     <>
-    
-    <Text color='gray.600' fontSize='sm' marginBottom={2}>تعداد رکورد :  {totalHint}</Text>
-    <SimpleGrid columns={{ xl: 6, lg: 5, md: 4, mdDown: 2 }} marginLeft={5} gap={5}>
-      {isPending &&
-        Skeletons.map((skeleton) => (
-          <MovieCardContainer key={skeleton}>
-            <MovieSkeleon />
-          </MovieCardContainer>
-        ))}
-      {MovieResult?.map(
-        (movie) =>
-          movie.source.is_active == 1 && (
-            <MovieCardContainer key={movie.documentId}>
-              <MovieCard movie={movie.source} />
+      
+      <Text color='gray.600' fontSize='sm' marginBottom={2}>تعداد رکورد :  </Text>
+      <SimpleGrid
+        columns={{ xl: 6, lg: 5, md: 4, mdDown: 2 }}
+        marginLeft={5}
+        gap={5}
+      >
+        {isPending &&
+          Skeletons.map((skeleton) => (
+            <MovieCardContainer key={skeleton}>
+              <MovieSkeleon />
             </MovieCardContainer>
-          )
-      )}
-    </SimpleGrid>
+          ))}
+        
+        {data?.pages.map((page, index) => (
+          
+          <React.Fragment key={index}>
+            {page.results.map(
+              (movie) =>
+                movie.source.is_active == 1 && (
+                  <MovieCardContainer key={movie.documentId}>
+                    <MovieCard movie={movie.source} />
+                  </MovieCardContainer>
+                )
+            )}
+          </React.Fragment>
+        ))}
+      </SimpleGrid>
+      {hasNextPage  &&  <Button onClick={()=>fetchNextPage()}>{isFetching ? 'در حال بارگذاری ...' : 'بیشتر'} </Button> }
+      
     </>
   );
 };
